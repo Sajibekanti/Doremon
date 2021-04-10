5a3a270f653416426ffc1b512df180ddc88a5b47
Sajibe Kanti <Sajibekanti@users.noreply.github.com>
2021-04-10 20:47:42 +0600

    Update doraemon.py

--- doraemon.py
+++ doraemon.py
@@ -306,5 +306,5 @@ if __name__ == '__main__':
     speed(9)    
     Doraemon()
     my_goto(100, -300)
-    write('by Sajibe Kanti (191-15-12174)', font=("Bradley Hand ITC", 30, "bold"))
+    write('by Sajibe Kanti', font=("Bradley Hand ITC", 30, "bold"))
     mainloop()
